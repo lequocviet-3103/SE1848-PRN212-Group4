ba913db81ea487e75efdcdce907b02d6d6ad262f
Tunna-github <trungvungoc2906@gmail.com>
2025-07-09 10:33:57 +0700

    cap nhat wpf

---
+++ BusinessObject/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Source { get; set; }
+        public string ImageUrl { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ DNASystem/LoginWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DNASystem
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrangChu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chkGhiNhoDangNhap_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDichVu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVeChungToi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBlog_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLienHe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDangNhap_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDangKy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnQuenMatKhau_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGoogle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFacebook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGioiThieu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTinTuc_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnChinhSach_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
