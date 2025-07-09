88789a1372344d2f4d70aa5e17f20939fdbe2e95
Tunna-github <trungvungoc2906@gmail.com>
2025-07-09 12:18:29 +0700

    Full WPF

---
+++ DNASystem/AboutUsWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DNASystem
+{
+    /// <summary>
+    /// Interaction logic for AboutUsWindow.xaml
+    /// </summary>
+    public partial class AboutUsWindow : Window
+    {
+        public AboutUsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrangChu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDichVu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVeChungToi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBlog_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLienHe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDangNhap_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDangKy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnXemDichVu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
