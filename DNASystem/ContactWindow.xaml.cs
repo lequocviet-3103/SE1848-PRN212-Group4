305465006199a2361dc75a426fba8ab426575b54
Tunna-github <trungvungoc2906@gmail.com>
2025-07-09 11:15:29 +0700

    them ContactForm

---
+++ DNASystem/ContactWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DNASystem
+{
+    /// <summary>
+    /// Interaction logic for ContactWindow.xaml
+    /// </summary>
+    public partial class ContactWindow : Window
+    {
+        public ContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrangChu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDichVu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnVeChungToi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBlog_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLienHe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDangNhap_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDangKy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGuiThongTin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnChiDuong_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGoiNgay_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
